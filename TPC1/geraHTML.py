bc24a3a769364e34e28bdf94c1928999af415b95
simaobarroso <simao.barroso@outlook.pt>
2023-02-21 15:44:40 +0000

    Spanish Inquisition

---
+++ TPC1/geraHTML.py
@@ -0,0 +1,15 @@
+from random import randint
+
+
+# ficheiro criado para demonstrar o que foi aprendido na primeira aula pratica
+# geracao de ficheiros HTML a partir de python
+
+listaRandom = ['''<iframe width="560" height="315" src="https://www.youtube.com/embed/yKQ_sQKBASM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>''' ,
+               '''<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>''',
+               '''<iframe width="560" height="315" src="https://www.youtube.com/embed/T8XeDvKqI4E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>''',
+               '''<iframe width="560" height="315" src="https://www.youtube.com/embed/iSmkqocn0oQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>''',
+               '''<iframe width="560" height="315" src="https://www.youtube.com/embed/f_6YwTLXqXw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>''',
+               '''<iframe width="560" height="315" src="https://www.youtube.com/embed/tas0O586t80" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'''
+               ]
+
+#print(listaRandom[randint(0,5)])               
