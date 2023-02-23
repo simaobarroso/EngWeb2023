c5572a5dc380acd9911bbb9b3f771d223a88dca3
simaobarroso <simao.barroso@outlook.pt>
2023-02-23 18:11:58 +0000

    pagina index feita

--- TPC2/geraHTMLindex.py
+++ TPC2/geraHTMLindex.py
@@ -1,5 +1,5 @@
 import json
-
+import time
 
 def ordCidade(c):
     return c['nome']
@@ -10,3 +10,46 @@ mapa = json.load(f)
 cidades = mapa["cidades"]
 ligações = mapa["ligações"]
 cidades.sort(key=ordCidade)
+
+#print(cidades)
+
+
+pagHTML = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TPC2</title>
+        <meta charset = "utf-8"/>
+    </head>
+    <body>
+    <h3>Distritos</h3>
+"""
+
+# pensar em maneiras mais eficientes
+# github
+distritos = dict()
+
+for c in cidades:
+    if c['distrito'] in distritos.keys():
+        distritos[c['distrito']] += [(c['nome'],c['id'])]
+
+    else :
+        distritos.update({c['distrito'] : [(c['nome'],c['id'])]})
+ 
+
+
+#colocar os distritos por ordem alfabetica
+sorted_keys = sorted(distritos.keys())
+distritos = {key:distritos[key] for key in sorted_keys}
+#tratar do caso de Evora estar em ultimo por causa do acento
+
+for d in distritos:
+    pagHTML += f"""
+        <h4>{d}</h4><ul>"""
+    for n in distritos[d]:
+        pagHTML += f"""<li><a href="{n[1]}">{n[0]}</a></li>
+        """       
+    pagHTML += """</ul>"""
+
+print(pagHTML)
+
