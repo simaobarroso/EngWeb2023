d1837d938ce29568167c8ad0f7afef0b68e8bd55
simaobarroso <simao.barroso@outlook.pt>
2023-02-27 22:40:37 +0000

    melhorias

--- TPC2/cidades/geraHTMLcid.py
+++ TPC2/cidades/geraHTMLcid.py
@@ -15,55 +15,32 @@ cidades.sort(key=ordCidade)
 
 collator = icu.Collator.createInstance(icu.Locale('pt_PT.UTF-8'))
 
+lig = dict()
+cities = dict()
 
-#cid = dict()
+for c in cidades:
+    cities.update({c['id']:c['nome']})
+    lig.update({c['id']: list()})
 
-# Assumo que o dataset nao tem ligacoes erradas nem o mesmo id para cidades diferentes
-
-# otimizacao?
-# percorro a mesma estrutura de dados varias vezes? big o
-#for c in cidades:
-    # if c['id'] in cid.keys() : print("Erro Cidade repetida") # ou usar um pass, para ignorar e apenas ter em considerecao a primeira
-    # dependia do metodo logo nao considerei
-#    cid.update({c['id'] : c['nome'] })
-
-
-# exemplo :
-# 'c34': 'Esposende', 'c31': 'Estarreja',
-
-
-#lig = dict() # ter um dicionario com todas
-
-# para as ligacoes serem bidirecionais
-#for l in ligações:
-#    if cid[l['origem']] in lig.keys():
-#        lig[cid[l['origem']]] += [(l['destino'],cid[l['destino']],l['distância'])]
-#    else :
-#        lig.update({cid[l['origem']] : [(l['destino'],cid[l['destino']],l['distância'])]})       
-    # vamos fazer las biderecionais
-#    if cid[l['destino']] in lig.keys():
-#        lig[cid[l['destino']]] += [(l['origem'], cid[l['origem']],l['distância'])]
-#    else :
-#        lig.update({cid[l['destino']] : [(l['origem'], cid[l['origem']],l['distância'])]})       
-
-# simplificar isto
-
- 
-
-#for elem in lig:
-    #sorted(lig[elem],key=itemgetter(1))
-#    lig[elem].sort(key = lambda x:x[1])
-    #print(lig[elem])
-    #time.sleep(2)
+if 1 == 1:
+    for l in ligações:
+        o = l['origem']
+        dest = l['destino']
+        dist = l['distância']
+        for c in cities:
+            #print(c)
+            if c == o and (cities[o],str(dist)) not in lig[c] :
+                #print("ola")
+                lig[c] += [(cities[dest],str(dist))]
+            elif c == o and (cities[o],str(dist)) not in lig[c]:
+                #print("adeus")
+                lig[c] += [(cities[o],str(dist))]     
+            
+for l in lig:
+    array = lig[l]
+    array.sort(key=lambda x: x[0])
 
 
-#print(lig)
-
-# exemplo :
-# 'Braga' : [('Lisboa',250), ...]
-
-
-# compliquei demais ? muito provalvemente
 
 
 for c in cidades:
@@ -80,7 +57,7 @@ for c in cidades:
                 </center>
             """    
     nomefich = str(c['id'])+".html"
-    f = open(nomefich,"x")    
+    f = open(nomefich,"w")    
     pagHTML += f"""
                             <a name="{c['id']}"></a>
                             <h3>{c['nome']}</h3>
@@ -88,25 +65,25 @@ for c in cidades:
                             <p><b>Descrição:</b>{c['descrição']}</p>
                             <p><b>Distrito:</b>{c['distrito']}</p>
                             <h3> Ligações:</h3>
+                            <ul>
                 """
     #ligs = lig[c['nome']]        
     for l in ligações:
         if l['origem'] == c['id']:
             for s in cidades:
                 if s['id']==l['destino']:
-                    pagHTML += f""" <ul>
+                    pagHTML += f""" 
                         <li><a href="#{s['id']}">{s['nome']}</a> - {l['distância']} Km</li>
-                    </ul>
-        
         """
+
         elif l["destino"] == c["id"]:
             for s in cidades:
                 if s['id']==l['origem']:
-                    pagHTML += f""" <ul>
+                    pagHTML += f""" 
                         <li><a href="#{s['id']}">{s['nome']}</a> - {l['distância']} Km</li>
-                    </ul>
-        """
-    pagHTML += f"""
+
+    `"""
+    pagHTML += f"""</ul>
                     <adress><a href="/"> [voltar ao Indice] </a>                
                         </body>
                     </html>"""
