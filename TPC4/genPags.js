a082c9c9709409a49afb588783a5636d578abf22
simaobarroso <simao.barroso@outlook.pt>
2023-03-09 11:43:29 +0000

    TPC4 : Inicio da estrutura

---
+++ TPC4/genPags.js
@@ -0,0 +1,5 @@
+// Aqui vao ser precisas ser geradas duas paginas, 
+// uma que e` a pagina principal, seguindo a estrutura dita na aula
+// uma de criar um utilizador
+
+// usar css para estrutura
---
+++ TPC4/server.js
@@ -0,0 +1,2 @@
+// server que vai ter receber pedidos post get, etc 
+// ver exemplo da aula
