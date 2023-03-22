7e8d60b0d70b3ff55722eb4546784fcc50937e22
simaobarroso <simao.barroso@outlook.pt>
2023-03-22 15:25:39 +0000

    1 e 2 parte feitas

--- TPC6/routes/index.js
+++ TPC6/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Pessoa = require('../controllers/pessoa')
 
-/* GET home page. */
-router.get('/pessoa', function(req, res, next) {
+
+router.get('/pessoas', function(req, res, next) {
   Pessoa.list()
     .then(dados => {
       res.json(dados);
@@ -12,7 +12,7 @@ router.get('/pessoa', function(req, res, next) {
   
 });
 
-router.get('/pessoa/:id', function(req, res, next) {
+router.get('/pessoas/:id', function(req, res, next) {
   Pessoa.getPessoa(req.params.id)
     .then(dados => {
       res.json(dados);
@@ -23,21 +23,21 @@ router.get('/pessoa/:id', function(req, res, next) {
 
 
 // temos que testar com o postman (ou curl)
-router.post('/pessoa',(req,res) => {
+router.post('/pessoas',(req,res) => {
   Pessoa.addPessoa(req.body)
     .then(dados => res.status(201).json(dados))
     .catch(erro => res.status(603).json({erro:erro}))
 
 })
 
-router.put('/pessoa/:id',(req,res) => {
+router.put('/pessoas/:id',(req,res) => {
   Pessoa.updatePessoa(req.body)
     .then(dados => res.json(dados))
     .catch(erro => res.status(604).json({erro:erro}))
 
 })
 
-router.delete('/treinos/:id',(req,res) => {
+router.delete('/pessoas/:id',(req,res) => {
   Pessoa.deletePessoa(req.params.id)
     .then(dados => res.json(dados))
     .catch(erro => res.status(605).json({erro:erro}))
